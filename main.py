bcd1d2b7823b14d34933eb8c47f66b2c31701e41
chrispo <69305550+chrispo-git@users.noreply.github.com>
2023-08-28 21:55:19 +0100

    Prototype 2

--- main.py
+++ main.py
@@ -17,8 +17,15 @@ FRAME_LENGTH = 60.0
 
 
 #Character constants
-WALK_SPEED = 12.0
 SCALE = 5
+
+WALK_SPEED = 12.0
+
+JUMP_DURATION_MUL = 1.5
+JUMP_INITAL = 20.0*JUMP_DURATION_MUL
+GRAVITY = 2.0*JUMP_DURATION_MUL
+MAX_FALL = 20.0*JUMP_DURATION_MUL
+AIR_MOVE = 10.0*JUMP_DURATION_MUL
 
 ANIMATION_LIST = [
     
@@ -65,6 +72,18 @@ ANIMATION_LIST = [
         ["sprites/F00_Crouch_1.gif", [-11.5,11.5,-25,2], None],
         ["sprites/F00_Crouch_0.gif", [-11.5,11.5,-25,14], None],
         ["sprites/Idle_0.gif", [-11.5,11.5,-25,14], None]
+    ],
+    [ #JumpSquat
+        ["sprites/F00_JumpSquat_0.gif", [-11.5,11.5,-25,14], None],
+        ["sprites/F00_JumpSquat_0.gif", [-11.5,11.5,-25,14], None]
+    ],
+    [ #Air
+        ["sprites/F00_Air_0.gif", [-11.5,11.5,-25,14], None],
+        ["sprites/F00_Air_0.gif", [-11.5,11.5,-25,14], None],
+        ["sprites/F00_Air_1.gif", [-11.5,11.5,-25,14], None],
+        ["sprites/F00_Air_2.gif", [-11.5,11.5,-25,14], None],
+        ["sprites/F00_Air_2.gif", [-11.5,11.5,-25,14], None],
+        ["sprites/F00_Air_1.gif", [-11.5,11.5,-25,14], None]
     ]
 ]
 
@@ -77,11 +96,14 @@ ANIMATION_LIST_LABEL = [
     "WalkB",
     "Crouch",
     "CrouchWait",
-    "CrouchRv"
+    "CrouchRv",
+    "JumpSquat",
+    "Air"
 ]
 
 #Collisions!
 PUSHBOXES = [11.5,-25,14]
+PUSHING_FORCE = 5
 char_pos = [[0,0], [0,0]]
 hurtbox = []
 
@@ -120,7 +142,6 @@ class player:
         self.playerNum = playerNum
         self.x = x
         self.y = y
-        self.isJump = False
         self.isHitstun = False
         self.is_left = Left
         self.isCrouch = False
@@ -131,6 +152,13 @@ class player:
         self.frame = 0
         self.animListID = 0
         self.sprite = "sprites/Idle_0.gif"
+
+        #Jump Attributes
+        self.isJump = False
+        self.lastmoveY = 0.0
+        self.lastmoveX = 0.0
+        self.lastmoveX = 0.0
+        self.jumpDir = 0.0 #-1.0 is back, 0.0 is neutral, 1.0 is forwards
 
         #Turtle Setup
         self.turtle = turtle.Turtle()
@@ -144,6 +172,7 @@ class player:
 
     def animate(self):
         global ANIMATION_LIST
+        global JUMP_INITAL
         anim = ANIMATION_LIST[self.animListID]
         anim_length = len(anim)
         sprite = anim[self.frame][0]
@@ -154,9 +183,12 @@ class player:
         self.turtle.shape(self.sprite)
         #print(self.turtle.shape())
         
-        if self.frame+1 >= anim_length:
+        if self.frame+1 >= anim_length: #Returns 
             self.frame = 0
             if self.isJump:
+                if self.animListID == get_anim_ID("JumpSquat"):
+                    self.moveYThisFrame = JUMP_INITAL
+                self.set_new_anim_by_ID(get_anim_ID("Air"))
                 return
             if self.isCrouch:
                 self.set_new_anim_by_ID(get_anim_ID("CrouchWait"))
@@ -218,6 +250,7 @@ class player:
             if self.isCrouch == False and self.animListID in [get_anim_ID("Idle"), get_anim_ID("WalkF"), get_anim_ID("WalkB")]:
                 self.isCrouch = True
                 self.set_new_anim_by_ID(get_anim_ID("Crouch"))
+
                 
     def uncrouch(self):
         global ANIMATION_LIST
@@ -225,6 +258,41 @@ class player:
         if self.isCrouch == True and self.animListID in [get_anim_ID("CrouchWait")]:
                 self.isCrouch = False
                 self.set_new_anim_by_ID(get_anim_ID("CrouchRv"))
+
+                
+    def up(self):
+        global ANIMATION_LIST
+        #print(f"[{self.x}, {self.y}]")
+        if self.isJump == False:
+            if self.animListID in [get_anim_ID("Idle"), get_anim_ID("WalkF"), get_anim_ID("WalkB")]:
+                self.isJump = True
+                self.set_new_anim_by_ID(get_anim_ID("JumpSquat"))
+    
+    def air(self):
+        global ANIMATION_LIST
+        global JUMP_INITAL
+        global GRAVITY
+        global MAX_FALL
+        global AIR_MOVE
+        #print(f"[{self.x}, {self.y}]")
+        if self.isJump == True:
+            if self.y <= 0.0 and self.lastmoveY < 0.0:
+                self.isJump = False
+                if self.y < 0.0:
+                    self.moveYThisFrame = -self.y
+                self.set_new_anim_by_ID()
+            else:
+                if self.animListID == get_anim_ID("JumpSquat"):
+                    if keyboard.is_pressed(self.controls[0]) and not keyboard.is_pressed(self.controls[1]):
+                        self.jumpDir = 1.0
+                    elif keyboard.is_pressed(self.controls[1]) and not keyboard.is_pressed(self.controls[0]):
+                        self.jumpDir = -1.0
+                    else:
+                        self.jumpDir = 0.0
+                else:
+                    self.moveYThisFrame = self.lastmoveY - GRAVITY
+                    self.moveXThisFrame = self.jumpDir * AIR_MOVE * -1.0
+
     
     def check_correct_side(self):
         global ANIMATION_LIST
@@ -240,8 +308,10 @@ class player:
     
     def update(self):
             global PUSHBOXES
+            global PUSHING_FORCE
 
             self.check_correct_side()
+            self.air()
             self.animate()
 
             if keyboard.is_pressed(self.controls[0]) and not keyboard.is_pressed(self.controls[1]):
@@ -260,36 +330,51 @@ class player:
                 self.down()
             elif self.isCrouch:
                 self.uncrouch()
+            if keyboard.is_pressed(self.controls[2]) and not keyboard.is_pressed(self.controls[3]):
+                self.up()
 
 
-            if self.moveXThisFrame != 0 or self.moveYThisFrame != 0:
+            if True:#self.moveXThisFrame != 0 or self.moveYThisFrame != 0:
                 x = self.x
                 y = self.y
                 newXVal = x
                 newYVal = y
+                #print(f"{newXVal}, {newYVal}")
+
+                #Forces players to stay within fighting area
                 if abs(x+self.moveXThisFrame) < BOX_SIZE[0]:
                     newXVal = x+self.moveXThisFrame
                 if y+self.moveXThisFrame >= 0:
                     newYVal = y+self.moveYThisFrame
-                #print(f"{newXVal}, {newYVal}")
 
                 #Pushbox
                 left_pushbox = (char_pos[-(self.playerNum)][0] - PUSHBOXES[0]) - self.x  < 105 and self.is_left == False
                 right_pushbox = self.x - (char_pos[-(self.playerNum)][0] + PUSHBOXES[0])  < 105 and self.is_left
-                top_pushbox = self.y - (char_pos[-(self.playerNum)][1] + PUSHBOXES[2])  < 105
+                top_pushbox = self.y - (char_pos[-(self.playerNum)][1] + PUSHBOXES[2])  < 55
                 bottom_pushbox =  (char_pos[-(self.playerNum)][1] + PUSHBOXES[1]) - self.y  < 105
                 #if top_pushbox and bottom_pushbox:
                     #print(f"same height: [{(char_pos[-(self.playerNum)][0] - PUSHBOXES[0]) - self.x}, {self.x - (char_pos[-(self.playerNum)][0] + PUSHBOXES[0])}]")
                     #print(f"X Pos for other one: {char_pos[-(self.playerNum)][0]}")
                 if (left_pushbox or right_pushbox) and (top_pushbox and bottom_pushbox):
                     #print("pushing!")
-                    if (self.moveXThisFrame > 0 and left_pushbox) or (self.moveXThisFrame < 0 and right_pushbox):
+                    if ((self.moveXThisFrame > 0 and left_pushbox) or (self.moveXThisFrame < 0 and right_pushbox)):
                         newXVal = x
+
+                    if (abs(char_pos[0][0]- char_pos[1][0]) < 104):
+                        push_off = PUSHING_FORCE
+                        if not self.is_left:
+                            push_off *= -1
+                        newXVal = x
+                        newXVal += push_off
+                
                     
 
                 self.x = newXVal
                 self.y = newYVal
                 self.turtle.setpos(self.x, self.y)
+                self.lastmoveX = newXVal - x
+                self.lastmoveY = newYVal - y
+                #print(f"{self.lastmoveX}, {self.lastmoveY}")
                 self.moveXThisFrame = 0.0
                 self.moveYThisFrame = 0.0
 
